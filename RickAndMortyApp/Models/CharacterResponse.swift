b2fb62bf60d730e34a2514e0cab67a3f8901b02a
Blessy Elizabeth Saini <>
2022-04-27 01:18:24 +0400

    removed unnecessary codes

--- RickAndMortyApp/Models/CharacterResponse.swift
+++ RickAndMortyApp/Models/CharacterResponse.swift
@@ -18,7 +18,7 @@ struct CharacterResponse: Codable {
 struct Info: Codable {
     let count, pages: Int?
     let next: String?
-    let prev: JSONNull?
+
 }
 
 // MARK: - Result
@@ -58,29 +58,4 @@ enum Status: String, Codable {
     case unknown = "unknown"
 }
 
-// MARK: - Encode/decode helpers
 
-class JSONNull: Codable, Hashable {
-
-    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
-        return true
-    }
-
-    public var hashValue: Int {
-        return 0
-    }
-
-    public init() {}
-
-    public required init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        if !container.decodeNil() {
-            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
-        }
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        try container.encodeNil()
-    }
-}
